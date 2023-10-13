af1b3bd9eb562e5e4832a8c5c8866f511a573a99
Matheus Pimentel Leal <matheusp.eng@gmail.com>
2023-10-13 00:23:55 -0300

    Add characters folder
    change project settings and create a basic movement script

---
+++ models/characters/Character.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+@export var speed = 100 # Player speed in Pixels / sec
+var screen_size # Actual size of the screen
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		print("Moving right")
+		velocity.x += 1
+	elif Input.is_action_pressed("move_left"):
+		print("Moving left")
+		velocity.x -= 1
+	elif  Input.is_action_pressed("jump"):
+		print("Jumping")
+		velocity.y += 1
+	elif Input.is_action_pressed("crouch"):
+		print("Crouching")
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed 
+
+	position = velocity * delta	
+	position = position.clamp(Vector2.ZERO, screen_size)
