4cbaf0e8e703e2aa3a59e1c5db02fbacfbaa8264
Matheus Pimentel Leal <matheusp.eng@gmail.com>
2023-10-20 18:37:42 -0300

    Refactor test scene to improve performance
    Removed unnecessary tiles

--- models/characters/Character.gd
+++ models/characters/Character.gd
@@ -10,20 +10,26 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_speed
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-
-	move_and_slide()
+  
+  # Add the gravity.
+  if not is_on_floor():
+	  velocity.y += gravity * delta
+  
+  # Handle Jump.
+  if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	  velocity.y = jump_speed
+  
+  # Get the input direction and handle the movement/deceleration.
+  # As good practice, you should replace UI actions with custom gameplay actions.
+  var direction = Input.get_axis("ui_left", "ui_right")
+  
+  if direction:
+	  velocity.x = direction * speed
+  else:
+	  velocity.x = move_toward(velocity.x, 0, speed)
+  
+  # This returns the KinematicCollision2D object that collided with the player, if none, returns null
+  var collision = move_and_collide(velocity * delta)
+  
+  if collision:
+	  print("Player collided with: ", collision.get_collider().name)
