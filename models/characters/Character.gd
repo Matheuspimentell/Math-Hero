e6fb8699361ccef093090a986a2297a66a1d9ac3
Matheus Pimentel Leal <matheusp.eng@gmail.com>
2023-10-22 11:14:10 -0300

    Add test scene and initial movement tests

--- models/characters/Character.gd
+++ models/characters/Character.gd
@@ -1,35 +1,31 @@
 extends CharacterBody2D
 signal hit
 
-
 @export var speed = 300.0
-@export var jump_speed = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-  
-  # Add the gravity.
-  if not is_on_floor():
-    velocity.y += gravity * delta
-  
-  # Handle Jump.
-  if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-    velocity.y = jump_speed
-  
-  # Get the input direction and handle the movement/deceleration.
-  # As good practice, you should replace UI actions with custom gameplay actions.
-  var direction = Input.get_axis("ui_left", "ui_right")
-  
-  if direction:
-    velocity.x = direction * speed
-  else:
-    velocity.x = move_toward(velocity.x, 0, speed)
-  
-  # This returns the KinematicCollision2D object that collided with the player, if none, returns null
-  var collision = move_and_collide(velocity * delta)
-  
-  if collision:
-    print("Player collided with: ", collision.get_collider().name)
+
+	# Get the input direction and handle the movement/deceleration.
+	var directionX = Input.get_axis("move_left", "move_right")
+	var directionY = Input.get_axis("move_up", "move_down")
+
+
+	if directionX:
+		velocity.x = directionX * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	if directionY:
+		velocity.y = directionY * speed
+	else:
+		velocity.y = move_toward(velocity.y, 0, speed)
+
+	# This returns the KinematicCollision2D object that collided with the player, if none, returns null
+	var collision = move_and_collide(velocity * delta)
+
+	if collision:
+		print("Player collided with: ", collision.get_collider().name)
