fa8d6415502047d994db39959eb52c11183f751c
Matheus Pimentel <matheusp.eng@gmail.com>
2024-04-11 14:08:59 -0300

    Feature: Added new input map prototype
    Will change later for a better implementation of an input map

--- models/operations/time_attack.gd
+++ models/operations/time_attack.gd
@@ -9,3 +9,21 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed('numpad0'):
 		numpad.find_child('number0').find_child('AnimationPlayer').playAnimation('click')
+	if Input.is_action_just_pressed('numpad1'):
+		numpad.find_child('number1').find_child('AnimationPlayer').playAnimation('click')
+	if Input.is_action_just_pressed('numpad2'):
+		numpad.find_child('number2').find_child('AnimationPlayer').playAnimation('click')
+	if Input.is_action_just_pressed('numpad3'):
+		numpad.find_child('number3').find_child('AnimationPlayer').playAnimation('click')
+	if Input.is_action_just_pressed('numpad4'):
+		numpad.find_child('number4').find_child('AnimationPlayer').playAnimation('click')
+	if Input.is_action_just_pressed('numpad5'):
+		numpad.find_child('number5').find_child('AnimationPlayer').playAnimation('click')
+	if Input.is_action_just_pressed('numpad6'):
+		numpad.find_child('number6').find_child('AnimationPlayer').playAnimation('click')
+	if Input.is_action_just_pressed('numpad7'):
+		numpad.find_child('number7').find_child('AnimationPlayer').playAnimation('click')
+	if Input.is_action_just_pressed('numpad8'):
+		numpad.find_child('number8').find_child('AnimationPlayer').playAnimation('click')
+	if Input.is_action_just_pressed('numpad9'):
+		numpad.find_child('number9').find_child('AnimationPlayer').playAnimation('click')
