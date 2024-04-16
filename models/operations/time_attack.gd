92f235cf30e329d1b94eb1b7be434bed4a20b4b4
Matheus Pimentel <matheusp.eng@gmail.com>
2024-04-16 11:53:44 -0300

    Fix: Fixed induced animation lag
    Removed 0.2 time period between activation and animation play

--- models/operations/time_attack.gd
+++ models/operations/time_attack.gd
@@ -6,6 +6,26 @@ extends Control
 func _input(event):
 	if event.is_action_pressed("number0"):
 		numpad.find_child("number0").find_child("AnimationPlayer").playAnimation("click")
+	if event.is_action_pressed("number1"):
+		numpad.find_child("number1").find_child("AnimationPlayer").playAnimation("click")
+	if event.is_action_pressed("number2"):
+		numpad.find_child("number2").find_child("AnimationPlayer").playAnimation("click")
+	if event.is_action_pressed("number3"):
+		numpad.find_child("number3").find_child("AnimationPlayer").playAnimation("click")
+	if event.is_action_pressed("number4"):
+		numpad.find_child("number4").find_child("AnimationPlayer").playAnimation("click")
+	if event.is_action_pressed("number5"):
+		numpad.find_child("number5").find_child("AnimationPlayer").playAnimation("click")
+	if event.is_action_pressed("number5"):
+		numpad.find_child("number5").find_child("AnimationPlayer").playAnimation("click")
+	if event.is_action_pressed("number6"):
+		numpad.find_child("number6").find_child("AnimationPlayer").playAnimation("click")
+	if event.is_action_pressed("number7"):
+		numpad.find_child("number7").find_child("AnimationPlayer").playAnimation("click")
+	if event.is_action_pressed("number8"):
+		numpad.find_child("number8").find_child("AnimationPlayer").playAnimation("click")
+	if event.is_action_pressed("number9"):
+		numpad.find_child("number9").find_child("AnimationPlayer").playAnimation("click")
 
 func _ready():
 	print(numpad.get_children())
