5986b7864637704ec7faa772201cd34f264bec07
Matheus Pimentel <matheusp.eng@gmail.com>
2024-05-20 15:43:55 -0300

    Feature: Added sounds

--- levels/scripts/password_screen.gd
+++ levels/scripts/password_screen.gd
@@ -1,66 +1,62 @@
 extends Node2D
 
 var numpad: Dictionary
+var sfxs: Dictionary
 @onready var question: RichTextLabel = $question
-@onready var password: Label = $passwordBox/password
+@onready var password: Label = $password_box/password
+@onready var sfx_controller: Node = $sfx_controller
 var sumGenerator = SumGenerator.new(null)
 var operations: Array
-var currentOp: int
+var _currentOp: int = 0
 
 func _ready():
 	# Get reference to all numpad keys
 	var numpadKeys = self.find_child("numpad").get_children()
 	for key in numpadKeys:
 		numpad[key.name] = key
+
 	operations = sumGenerator.gen_one_digit_restricted(10)
 	print(operations)
-	currentOp = 0
-	self._change_question_text('%s + %s = ?' % [operations[currentOp].a, operations[currentOp].b])
+	self._change_question_text('%s + %s = ?' % [operations[_currentOp].a, operations[_currentOp].b])
+
+	var sound_effects = sfx_controller.get_children()
+	for effect in sound_effects:
+		sfxs[effect.name] = effect
 
 func _input(_event):
 	if Input.is_action_just_pressed("number0"):
 		numpad["0"].click()
-		password.text += "0"
 	elif Input.is_action_just_pressed("number1"):
 		numpad["1"].click()
-		password.text += "1"
 	elif Input.is_action_just_pressed("number2"):
 		numpad["2"].click()
-		password.text += "2"
 	elif Input.is_action_just_pressed("number3"):
 		numpad["3"].click()
-		password.text += "3"
 	elif Input.is_action_just_pressed("number4"):
 		numpad["4"].click()
-		password.text += "4"
 	elif Input.is_action_just_pressed("number5"):
 		numpad["5"].click()
-		password.text += "5"
 	elif Input.is_action_just_pressed("number6"):
 		numpad["6"].click()
-		password.text += "6"
 	elif Input.is_action_just_pressed("number7"):
 		numpad["7"].click()
-		password.text += "7"
 	elif Input.is_action_just_pressed("number8"):
 		numpad["8"].click()
-		password.text += "8"
 	elif Input.is_action_just_pressed("number9"):
 		numpad["9"].click()
-		password.text += "9"
 	elif Input.is_action_just_pressed("backspace"):
 		numpad["backspace"].click()
 	elif Input.is_action_just_pressed("accept"):
-		if str(operations[currentOp].res) == password.text:
-			print('correct answer')
-			if(currentOp == operations.size()-1):
+		if str(operations[_currentOp].res) == password.text:
+			sfxs["correct_answer"].play()
+			if(_currentOp == operations.size()-1):
 				print('Congrats!')
 			else:
-				currentOp+=1
-				self._change_question_text('%s + %s = ?' % [operations[currentOp].a, operations[currentOp].b])
+				_currentOp+=1
+				self._change_question_text('%s + %s = ?' % [operations[_currentOp].a, operations[_currentOp].b])
 				password.text = ''
 		else:
-			print('wrong answer')
+			sfxs["wrong_answer"].play()
 			password.text = ''
 
 func _set_question_style(text: String):
--- models/numpad_key.gd
+++ models/numpad_key.gd
@@ -1,9 +1,22 @@
 extends AnimatedSprite2D
 class_name NumpadKey
+var password: Label
+var click_sound: AudioStreamPlayer
+var error_sound: AudioStreamPlayer
 
-
+func _ready():
+  password = get_node(^"../../password_box/password")
+  click_sound = get_node(^"../../sfx_controller/click")
+  error_sound = get_node(^"../../sfx_controller/error")
 
 func click():
-  self.play("click")
-  print("Button %s was clicked!" % self.name)
+  if self.name == 'backspace':
+    if(password.text.length() >= 1):
+      password.text = password.text.erase(password.text.length()-1, 1)
+      click_sound.play()
+    else:
+      error_sound.play()
+  else:
+    click_sound.play()
+    password.text += self.name
   
