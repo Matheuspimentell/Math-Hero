05735736025eead698d0911f16af01ef81bb1139
Matheus Pimentel Leal <matheusp.eng@gmail.com>
2024-08-07 20:19:15 -0300

    Feature: Add name insertion on time attack results

--- scripts/time_attack_results.gd
+++ scripts/time_attack_results.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var errors = $MarginContainer/background/MarginContainer/info/errors/error_counter
 @onready var penalties = $MarginContainer/background/MarginContainer/info/penalties/penalty_counter
 @onready var final_time_label = $MarginContainer/background/MarginContainer/info/final_time/total_time
+@onready var name_dialog = $MarginContainer/background/name_dialog
+@onready var player_name = $MarginContainer/background/name_dialog/VBoxContainer/LineEdit
 
 func _ready():
 	# Get all data from GameManager and perform calculations
@@ -11,7 +13,13 @@ func _ready():
 	errors.text = "%d" % [GameManager.tattack_results["errors"]] if GameManager.tattack_results.has("errors") else "0"
 	penalties.text = "+%d s" % [self.get_total_penalty_time()]
 	final_time_label.text = get_final_time()
-	
+
+func _input(event):
+	# If accept is pressed, show popup to input name
+	if event.is_action_released("accept"):
+		name_dialog.visible = true
+		player_name.grab_focus()
+
 func get_total_penalty_time() -> float:
 	return GameManager.tattack_results["errors"] * 30 if GameManager.tattack_results.has("errors") else 0
 
@@ -26,3 +34,6 @@ func get_final_time() -> String:
 	var penalty_time: float = get_total_penalty_time()
 	var final_time = format_time(time+penalty_time)
 	return final_time
+
+func _on_name_submitted(new_text:String):
+	# Change scene to leaderboard
